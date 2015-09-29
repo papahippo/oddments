aeed89b592384b355c9e4cd63814590f00b80a52
Larry Myerscough <hippos@chello.nl>
2015-09-29 11:19:43 +0200

    first commit to new repository

---
+++ to_and_fro.py
@@ -0,0 +1,43 @@
+#/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 17:59:40 2015 in answer to the question on Lined-in:
+LIST REORDER
+
+HI GUYES
+I AM A VERY BRGINNER IN PYTHON AND I HAVE A SMALL QUESTION :
+IF I HAVE A LIST OF STRINGS AND I WANT TO REARRANGE IT BY TAKING THE VERY Ist ELEMENT THEN THE VERY LAST ONE , THEN THE SECOND FROM THE BEGINNING AND THE SECOND FROM THE END AND SO ON ..
+WOULD YOU PLEASE GUIDE ME
+THANKS
+@author: Larry Myerscough (aka papahippo)
+"""
+
+def to_and_fro(ss):
+    """
+    'sort' a sequence (e.g. a list) of strings by taking 1st, then last,
+    then 2nd, then last-but-one and so on.
+    """
+    ss = ss[:]  # make sure we have a list; don't change callers's list.
+    ss_new = []
+    while ss:   # i.e. until there's no more left to take
+        ss_new.append(ss.pop(0))
+        if ss:
+            ss_new.append(ss.pop(-1))
+    return ss_new
+    
+# Above this line is the actual function.
+# Below this line is all test code.
+if __name__ == '__main__':
+    for list_size, reason in ((0, 'empty'),
+                              (4, 'even-sized'),
+                              (7, 'odd-sized')):
+        orig_list = ["    case '%s' string #%d" %(reason, i+1)
+                        for i in xrange(list_size)]
+        sorted_list = to_and_fro(orig_list)
+        print ("\n\ncase %s" % reason)
+        for (which, when) in ((orig_list, "before"),
+                              (sorted_list, "after")):
+            print ("\n  %s" % when)
+            print ("\n".join(which))
+
+        

21377fbb8c0dd7cdc0cae1f2818c23f4da7abcbd
Larry Myerscough <hippos@chello.nl>
2015-09-30 16:18:12 +0200

    added comments; test with lists and with tuples.

--- to_and_fro.py
+++ to_and_fro.py
@@ -17,7 +17,13 @@ def to_and_fro(ss):
     'sort' a sequence (e.g. a list) of strings by taking 1st, then last,
     then 2nd, then last-but-one and so on.
     """
-    ss = list(ss)  # make sure we have a list; don't change caller's original.
+    # The following statement is arguably unnecesary but has two advantages:
+    # 1. It ensures that the function still works if e.g. a tuple is passed
+    #    rather than a list; we need to be able to 'pop' elements.
+    # 2. It avoids destroying the caller's list. The caller could work around
+    #    such a 'feature' but it would nonetheless be bad style in my book.
+    #
+    ss = list(ss)
     ss_new = [] # start our answer as an empty list
     while ss:   # i.e. until there's no more left to take
         ss_new.append(ss.pop(0)) # take one from start:
@@ -28,18 +34,37 @@ def to_and_fro(ss):
     return ss_new
     
 # Above this line is the actual function.
+# --------------------------------------------------------------
 # Below this line is all test code.
 if __name__ == '__main__':
+    # I want to test with an empty list and with non-empty lists with odd and
+    # even lengths in order to believe 'to_and_fro' really works ok:
+    #
     for list_size, reason in ((0, 'empty'),
                               (4, 'even-sized'),
                               (7, 'odd-sized')):
-        orig_seq = ["    case '%s' string #%d" %(reason, i+1)
-                        for i in xrange(list_size)]
-        sorted_seq = to_and_fro(orig_seq)
-        print ("\n\ncase %s" % reason)
-        for (which, when) in ((orig_seq, "before"),
-                              (sorted_seq, "after")):
-            print ("\n  %s" % when)
-            print ("\n".join(which))
+        # I also want to test my claim that 'to_and_fro' works equally well for
+        # lists and tuples.
+        #
+        for use_tuple in (False, True):
+            # construct strings with easily recognizable order:
+            #
+            sequence_type_name = ('list', 'tuple')[use_tuple]
+            orig_seq = ["    case '%s' using %s - string #%d"
+                %(reason, sequence_type_name, i+1) for i in xrange(list_size)]
+            
+            # if one didn't need the original strings anymore, onecould assign
+            # e.g seq = to_and_fro(seq); but we want to test that it is
+            # possible to leave the original sequence unscathed.
+            #
+            if use_tuple:
+                orig_seq = tuple(orig_seq)
+            sorted_seq = to_and_fro(orig_seq)
+            print ("\n\ncase %s (using %ss of strings)"
+                   % (reason, sequence_type_name))
+            for (which, when) in ((orig_seq, "original"),
+                                  (sorted_seq, "to_and_fro sorted")):
+                print ("\n  %s" % when)
+                print ("\n".join(which))
 
         

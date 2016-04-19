e01b2e57e8db7354aa15850a3bc907477abf87cf
papahippo <hippos@chello.nl>
2016-04-19 20:52:01 +0200

    minor textual fixes.

--- LetterToA4.py
+++ LetterToA4.py
@@ -6,6 +6,7 @@ on the scanner I ensured no part of the image was lost... but I need to distribu
 Warnings:
      1. I added the sys.argc stuff just before posting on github without re-testing!
      2. The style does not conform to python commuity consensus of 'best practice'; e.g. %s stuff is old-fashioned.
+     3. i used  '.bmp' file by accident (I was borrrowing my son's scanner because it supports letter format better).
 """
 
 import sys,os
@@ -13,7 +14,7 @@ from PIL import Image, ImageOps
 from os import listdir
 from os.path import isfile, join
 
-print("running '%s' to convert pdf's from latter to A4" % sys.argv.pop(0))
+print("running '%s' to convert scanned images from 'letter' without margin to A4 with margin" % sys.argv.pop(0))
 mypath = (sys.argv and sys.argv.pop(0)) or '/home/gill/MEW Archive/Lord of the dance/ScannedAtColins/'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 myext = '.bmp'

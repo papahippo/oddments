dd3e4440139dd63fe7cf7227b993ed7406222fd5
papahippo <hippos@chello.nl>
2016-05-12 17:34:09 +0200

    now using ctypes, kind of based on how I did stuff for Antwerp Space.

--- pisdn.py
+++ pisdn.py
@@ -4,11 +4,24 @@ I'm incubating my python isdn monitor stuff here.. I'll move it toits own projec
 """
 import sys, os, socket, fcntl, array, ctypes
 
-class Packet_part(ctypes.BigEndianStructure):
+class EasyStructure(ctypes.LittleEndianStructure):
+    def __str__(self):
+        return '\n'.join(
+            ["%s = 0x%x" %(_f[0], getattr(self, _f[0]))
+             for _f in self._fields_]
+        )
+
+class ABI_version(EasyStructure):
     """
     """
     _pack_ = 1
-    _fields_ = []
+    _fields_ = [
+        ("major_version",       ctypes.c_uint32,  8),
+        ("minor_version",       ctypes.c_uint32,  8),
+        ("release",             ctypes.c_uint32, 14),
+        ("_spare_1",            ctypes.c_uint32,  1),
+        ("is_git.misdn.eu_ver", ctypes.c_uint32,  1),
+    ]
 
 
 socket.PF_ISDN = 34
@@ -18,6 +31,8 @@ print("running '%s' to monitor ISDN activity" % sys.argv.pop(0))
 mySocket = socket.socket(socket.PF_ISDN, socket.SOCK_RAW, 0)
 #print (dir(socket))
 print (mySocket)
-versionArray = array.array('b', [0]*8)
-print (fcntl.ioctl(mySocket, IMGETVERSION, versionArray))
-print (versionArray)
+mISDN_version = ABI_version() # array.array('b', [0]*8)
+print (fcntl.ioctl(mySocket, IMGETVERSION, mISDN_version))
+print (mISDN_version)
+#print (dir(mISDN_version))
+#print (mISDN_version)

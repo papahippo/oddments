2bd35544620fdb10fe5583bc635b2e165743f8fa
Larry Myerscough <hippos@chello.nl>
2016-05-16 17:08:25 +0200

    long move downstairs!

--- pymisdn.py
+++ pymisdn.py
@@ -4,16 +4,21 @@ I'm incubating my python isdn monitor stuff here.. I'll move it toits own projec
 """
 import sys, os, socket, fcntl, struct, array
 
+# def composeFuncCode(fucNum, )
+# ... on hold; currently using magic 32-bit constants!
+
+PF_ISDN = AF_ISDN = 34
+
 class ISocket(socket.socket):
     tag = 'I'
 
 
-    def __init__(self, family=34, type=socket.SOCK_RAW, proto=0, fileno=None):
+    def __init__(self, family=PF_ISDN, type=socket.SOCK_RAW, proto=0, fileno=None):
         socket.socket.__init__(self, family, type, proto, fileno)
 
-    def ioctl(self, funcNum, fmt, *values):
+    def ioctl(self, funcCode, fmt, *values):
         size = struct.calcsize(fmt)
-        funcCode = funcNum + (ord(self.tag)<<8) + (4<<16) + 0x80000000;
+        # funcCode = funcNum + (ord(self.tag)<<8) + (size<<16) + 0x80000000;
         buf = bytearray(size)
         if values:
             struct.pack_into(fmt, buf, 0, *values)
@@ -26,15 +31,13 @@ class ISocket(socket.socket):
         return struct.unpack(fmt, buf)
 
     def get_mISDN_Version(self):
-        return self.ioctl(66, 'BBH')
+        return self.ioctl(0x80044942, 'BBH')
 
     def get_device_count(self):
-        return self.ioctl(67, 'L')
+        return self.ioctl(0x80044943, 'L')
 
     def get_device_info(self, device_index):
-        return self.ioctl(68, 'LLLL16sL20s', device_index, 0, 0, 0,
-                          0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  0,
-                          0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, )
+        return self.ioctl(0x80044944, 'LLLL16sL20s', device_index, 0, 0, 0, bytes(' ', 'utf8'), 0, bytes('', 'utf8'))
 
 def main():
     iSocket = ISocket()
@@ -47,9 +50,27 @@ def main():
     for device_index in range(device_count):
         print ("device_index %s" % device_index)
         device_info = iSocket.get_device_info(device_index)
+        _id, Dprotocols, Bprotocols, protocol, bytes_channelMap, nrBchan, bytes_device_name = device_info
         print (device_info)
+        device_name = bytes_device_name.decode('utf8')
+        print(device_name)
+    iSocket.close()
 
-    mySocket.close()
+    iSocket = ISocket(PF_ISDN, socket.SOCK_DGRAM, protocol)
+
+    dch_echo = 0  # can be set to 1 by arg in loghex.c from which this is largely cribbed!
+    card_no = 0 # ditto
+    # try to bind on D/E channel first, fallback to D channel on error
+    #
+    for channel in reversed(list(range(dch_echo+1))):  # (1,0) or just (0,)
+        print(iSocket.bind((AF_ISDN, card_no)))
+        break
+
+
+
+
+    while (1):
+        break
 
 if __name__ == "__main__":
     main()

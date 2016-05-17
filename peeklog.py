315130962d26193c329ada61b5211dacc24f8198
papahippo <hippos@chello.nl>
2016-05-17 22:17:55 +0200

    parsing looks ok; not started cracking the mesage codes yet though!

--- peeklog.py
+++ peeklog.py
@@ -8,11 +8,12 @@ import sys, os, time, struct
 class EyeSDNFile:
     tell_tale_bytes = 'EyeSDN'.encode('utf8') + bytes([0xff])
 
-    def __init__(self, name, mode='rb', block_size=1024):
+    def __init__(self, name, mode='rb', block_size=1024, dither=False):
         if not 'b' in mode:
             raise ValueError("must use binary (e.g. 'rb' or 'wb') mode for EyeSDN files")
         self.actual_file = open(name, mode=mode)
         self.block_size = block_size
+        self.dither= dither
         if 'r' in mode:
             starter = self.actual_file.read(7)
             if  starter != self.tell_tale_bytes:
@@ -23,15 +24,46 @@ class EyeSDNFile:
 
     def read_packet(self):
         while 1:
-            i = self.carry.find(0xff)
+            i = self.carry_bytes.find(0xff)
+            print ("index of 0xff =", i)
             if i >= 0:
                 packet = self.carry_bytes[:i]
                 self.carry_bytes = self.carry_bytes[i+1:]
                 break
+            new_bytes = self.actual_file.read(self.block_size)
+            if new_bytes:
+                self.carry_bytes += new_bytes
+                continue
+            if not self.dither:
+                packet = self.carry_bytes
+                self.carry_bytes = b''
+                break
+            time.sleep(self.dither)
+
+        return (packet.replace(b'\xfe\xfd', b'\xff')
+                      .replace(b'\xfe\xfc', b'\xfe'))
+
+    def get_packet_parts(self):
+        packet = self.read_packet()
+        head = bytes([0]) + packet[:12]  # prefix null byte to facilitate unpacking
+        body = packet[12:]
+        print([hex(b) for b in head] + [' ---'] + [hex(b) for b in body])
+        usecs, secs, origin, length = struct.unpack('>LxLxbH', head)
+        if length != len(body):
+            raise ValueError('actual length %s != advertized lengh %s'
+                             %(len(body), length))
+        return time.localtime(secs), usecs, body
+
 def main():
     prog = sys.argv.pop(0)
     log =  EyeSDNFile(sys.argv and sys.argv.pop() or 'isdnws.log', 'rb')
-
+    while 1:
+        if 0:
+            packet = log.read_packet()
+            if not packet:
+                sys.exit(0)
+        local_time_then, usecs, body = log.get_packet_parts()
+        print(len(body))
 if __name__ == '__main__':
     main()
 

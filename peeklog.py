a993fb67a9a60eb6c190db04e1eae7dfcdc63cd9
papahippo <hippos@chello.nl>
2016-05-18 13:45:07 +0200

    logging now detects system resets (required doctored loghhex.c with -a [=append] option.

--- peeklog.py
+++ peeklog.py
@@ -6,7 +6,7 @@ import sys, os, time, struct
 
 
 class EyeSDNFile:
-    tell_tale_bytes = 'EyeSDN'.encode('utf8') + bytes([0xff])
+    tell_tale_bytes = 'EyeSDN'.encode('utf8')
 
     def __init__(self, name, mode='rb', block_size=1024, dither=False):
         if not 'b' in mode:
@@ -14,7 +14,7 @@ class EyeSDNFile:
         self.actual_file = open(name, mode=mode)
         self.block_size = block_size
         self.dither= dither
-        if 'r' in mode:
+        if 0: # 'r' in mode:
             starter = self.actual_file.read(7)
             if  starter != self.tell_tale_bytes:
                 raise IOError("file does not start with '%s'" % self.tell_tale_bytes)
@@ -39,18 +39,20 @@ class EyeSDNFile:
                 self.carry_bytes = b''
                 break
             time.sleep(self.dither)
-
+        # print ("carry bytes", self.carry_bytes)
         return (packet.replace(b'\xfe\xfd', b'\xff')
                       .replace(b'\xfe\xfc', b'\xfe'))
 
     def get_packet_parts(self):
         packet = self.read_packet()
+        if packet ==self.tell_tale_bytes:
+            return None
         head = bytes([0]) + packet[:12]  # prefix null byte to facilitate unpacking
         usecs, secs, origin, length = struct.unpack('>LxLxbH', head)
         local_time_then = time.localtime(secs)
         body = packet[12:]
-        print(time.strftime('%Y-%b-%d %H:%M:%S.', local_time_then) + ('%06u' % usecs))
-        print([hex(b) for b in head] + [' ---'] + [hex(b) for b in body])
+        # print(time.strftime('%Y-%b-%d %H:%M:%S.', local_time_then) + ('%06u' % usecs))
+        # print([hex(b) for b in head] + [' ---'] + [hex(b) for b in body])
         if length != len(body):
             raise ValueError('actual length %s != advertized lengh %s'
                              %(len(body), length))
@@ -58,9 +60,16 @@ class EyeSDNFile:
 
 def main():
     prog = sys.argv.pop(0)
-    log =  EyeSDNFile(sys.argv and sys.argv.pop() or 'isdnws.log', 'rb')
+    log =  EyeSDNFile(sys.argv and sys.argv.pop() or
+                      '/home/gill/isdn/mISDNuser-21b8c52/tools/wsa.log', 'rb', dither=0.2)
     while 1:
-        local_time_then, usecs, body = log.get_packet_parts()
-
+        packet_parts = log.get_packet_parts()
+        if not packet_parts:
+            print ("logger daemon (re-)started")
+            continue
+        local_time_then, usecs, body = packet_parts
+        print(time.strftime('%Y-%b-%d %H:%M:%S.', local_time_then) + ('%06u ---' % usecs),
+            *([hex(b) for b in body]))
+        #print(log.read_packet())
 if __name__ == '__main__':
     main()

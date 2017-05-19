67085885fd63ddcd691ee09c119d256c36108667
papahippo <hippos@chello.nl>
2017-05-19 20:10:22 +0200

    taking shape after stating life as the fractionslider (thanks!)

---
+++ bookmarkedslider.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+# widget derived from the BookmarkedSlider example of the pyqt tutorial book.
+
+import platform
+
+from PyQt4.QtCore import (QPointF, QRectF, QSize, Qt, pyqtSignal)
+from PyQt4.QtGui import (QApplication, QColor, QDialog, QFont,
+        QFontMetricsF, QGridLayout, QLCDNumber, QLabel, QPainter,
+        QPalette, QPolygonF, QSizePolicy, QSpinBox, QWidget)
+X11 = True
+try:
+    from PyQt4.QtGui import qt_x11_wait_for_window_manager
+except ImportError:
+    X11 = False
+
+
+class Widgelet:
+    """
+    A 'widgelet' is defined (by me!) as a component which can be added to a widget and has less autonomy than an
+    actual widget.
+    """
+    pass
+
+class Bookmark(Widgelet):
+
+    WIDTH = 12.0
+    HEIGHT = 10.0
+
+    def __init__(self, x, tag):
+        self.__x = x
+        self.__tag = tag
+
+    def paint(self, painter,):
+        painter.translate(self.__x, 0)
+        self.putShape(painter)
+        self.putTag(painter)
+        painter.translate(-self.__x, 0)
+
+    def putShape(self, painter):
+        triangle = [QPointF(-self.WIDTH/2, 0),
+                    QPointF(self.WIDTH/2, 0),
+                    QPointF(0, self.HEIGHT)]
+        painter.setPen(Qt.yellow)
+        painter.setBrush(Qt.darkYellow)
+        painter.drawPolygon(QPolygonF(triangle))
+
+
+    def putTag(self, painter):
+        pass  # for now!
+
+class BookmarkedSlider(QWidget):
+
+    XMARGIN = 12.0
+    YMARGIN = 5.0
+    WSTRING = "999"
+
+    valueChanged = pyqtSignal(int)
+
+    def __init__(self, lowest=0, highest=100, value=0, parent=None):
+        super(BookmarkedSlider, self).__init__(parent)
+        self.__lowest = lowest
+        self.__highest = highest
+        self.__value = value
+        self.setFocusPolicy(Qt.WheelFocus)
+        self.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding,
+                                       QSizePolicy.Fixed))
+        self.bookmarks = [Bookmark(54, 'test')]
+
+    def span(self):
+        return self.width() - (self.XMARGIN * 2)
+
+    def range(self):
+        return (self.__highest - self.__lowest)
+
+    def xFromValue(self, value):
+        return self.XMARGIN + (value*self.span()/self.range())
+
+    def valueFromX(self, x):
+        return (x-self.XMARGIN)*self.range()/self.span()
+
+    def sizeHint(self):
+        return self.minimumSizeHint()
+
+    def minimumSizeHint(self):
+        font = QFont(self.font())
+        font.setPointSize(font.pointSize() - 1)
+        fm = QFontMetricsF(font)
+        return QSize(# fm.width(BookmarkedSlider.WSTRING) *
+                     # (self.__highest - self.__lowest),
+                     480,
+                     (fm.height() * 2) + BookmarkedSlider.YMARGIN)
+
+    def setValue(self, value):
+        if self.__lowest <= value <= self.__highest:
+            self.__value = value
+        else:
+            raise ValueError("slider value out of range")
+        self.update()
+        self.updateGeometry()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.moveSlider(event.x())
+            event.accept()
+        else:
+            QWidget.mousePressEvent(self, event)
+
+    def mouseMoveEvent(self, event):
+        self.moveSlider(event.x())
+
+
+    def moveSlider(self, x):
+        value = self.valueFromX(x)
+        print(x, value)
+        if value != self.__value:
+            self.__value = value
+            self.valueChanged.emit(self.__value)
+            self.update()
+
+
+    def keyPressEvent(self, event):
+        change = 0
+        if event.key() == Qt.Key_Home:
+            change = self.__lowest - self.__value
+        elif event.key() in (Qt.Key_Up, Qt.Key_Right):
+            change = 1
+        elif event.key() == Qt.Key_PageUp:
+            change = 10
+        elif event.key() in (Qt.Key_Down, Qt.Key_Left):
+            change = -1
+        elif event.key() == Qt.Key_PageDown:
+            change = -10
+        elif event.key() == Qt.Key_End:
+            change = self.__highest - self.__value
+        if change:
+            value = self.__value
+            value += change
+            if value != self.__value:
+                self.__value = value
+                self.valueChanged.emit(self.__value)
+                self.update()
+            event.accept()
+        else:
+            QWidget.keyPressEvent(self, event)
+
+
+    def paintEvent(self, event=None):
+        font = QFont(self.font())
+        font.setPointSize(font.pointSize() - 1)
+        fm = QFontMetricsF(font)
+        fracWidth = fm.width(BookmarkedSlider.WSTRING)
+        indent = fm.boundingRect("9").width() / 2.0
+        if not X11:
+            fracWidth *= 1.5
+        value = self.__value
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setRenderHint(QPainter.TextAntialiasing)
+        painter.setPen(self.palette().color(QPalette.Mid))
+        painter.setBrush(self.palette().brush(
+                QPalette.AlternateBase))
+        painter.drawRect(self.rect())
+        segColor = QColor(Qt.green).dark(120)
+        segLineColor = segColor.dark()
+        painter.setPen(segLineColor)
+        painter.setBrush(segColor)
+        painter.drawRect(BookmarkedSlider.XMARGIN,
+                         BookmarkedSlider.YMARGIN, self.span(), fm.height())
+        textColor = self.palette().color(QPalette.Text)
+        segHeight = fm.height() * 2
+        nRect = fm.boundingRect(BookmarkedSlider.WSTRING)
+        x = BookmarkedSlider.XMARGIN
+        yOffset = 0 #  segHeight #  + fm.height()
+        painter.translate(100, 0)
+        painter.setPen(Qt.yellow)
+        painter.setBrush(Qt.darkYellow)
+        painter.drawRect(x, yOffset, self.xFromValue(value), fm.height())
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    form = QDialog()
+    # form.setMaximumWidth(80)
+    sliderLabel = QLabel("&Fraction?")
+    slider = BookmarkedSlider(value=42)
+    sliderLabel.setBuddy(slider)
+    layout = QGridLayout()
+    layout.addWidget(sliderLabel, 0, 0)
+    layout.addWidget(slider, 0, 1, 1, 3)
+    form.setLayout(layout)
+
+    def valueChanged(value):
+        slider.setValue(value)
+
+    form.setWindowTitle("Bookmarked Slider")
+    form.show()
+    app.exec_()
+

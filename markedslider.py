ca973cb9f8a8cbf13838c59ea138a4744c13852d
papahippo <hippos@chello.nl>
2017-06-05 19:45:00 +0200

    minor re-style to facilitate inclusion in musicraft

--- markedslider.py
+++ markedslider.py
@@ -1,44 +1,38 @@
 #!/usr/bin/env python
-# widget derived from the BookmarkedSlider example of the pyqt tutorial book.
-
+"""  Widget derived from the Fraction Slider example of the pyqt tutorial book.
+    Destined for use in my MusicRaft package
+"""
 import platform
 
-from PyQt4.QtCore import (QPointF, QRectF, QSize, Qt, pyqtSignal)
-from PyQt4.QtGui import (QApplication, QColor, QDialog, QFont,
-        QFontMetricsF, QGridLayout, QLCDNumber, QLabel, QPainter,
-        QPalette, QPolygonF, QSizePolicy, QSpinBox, QWidget)
-X11 = True
-try:
-    from PyQt4.QtGui import qt_x11_wait_for_window_manager
-except ImportError:
-    X11 = False
+from PyQt4 import (QtCore, QtGui)
 
+X11 = hasattr(QtGui, 'qt_x11_wait_for_window_manager')
 
-class Mark(QWidget):
+class Mark(QtGui.QWidget):
 
     WIDTH = 12.0
     HEIGHT = 10.0
 
     def __init__(self, parent=None, ix=0, tag='?', x=0, y=0):
-        QWidget.__init__(self, parent=parent)
+        QtGui.QWidget.__init__(self, parent=parent)
         self.ix = ix
         self.tag = tag
         self.setGeometry(x, y, self.WIDTH, self.HEIGHT)
         self.show()
 
     def paintEvent(self, event=None):
-        painter = QPainter(self)
+        painter = QtGui.QPainter(self)
         self.putShape(painter)
         self.putTag(painter)
 
 
     def putShape(self, painter):
-        triangle = [QPointF(0, 0),
-                    QPointF(self.WIDTH, 0.),
-                    QPointF(self.WIDTH/2, self.HEIGHT)]
-        painter.setPen(Qt.blue)
-        painter.setBrush(Qt.blue)
-        painter.drawPolygon(QPolygonF(triangle))
+        triangle = [QtCore.QPointF(0, 0),
+                    QtCore.QPointF(self.WIDTH, 0.),
+                    QtCore.QPointF(self.WIDTH/2, self.HEIGHT)]
+        painter.setPen(QtCore.Qt.blue)
+        painter.setBrush(QtCore.Qt.blue)
+        painter.drawPolygon(QtGui.QPolygonF(triangle))
 
 
     def putTag(self, painter):
@@ -63,35 +57,39 @@ class TimeMark(Mark):
     HEIGHT = 10.0
 
     def putShape(self, painter):
-        painter.setPen(Qt.red)
-        painter.setBrush(Qt.red)
+        painter.setPen(QtCore.Qt.red)
+        painter.setBrush(QtCore.Qt.red)
         painter.drawRect(0., 0., self.WIDTH, self.HEIGHT)
 
 
     def putTag(self, painter):
         pass  # for now!
 
-class MarkedSlider(QWidget):
+class MarkedSlider(QtGui.QWidget):
 
     XMARGIN = 12.0
     YMARGIN = 5.0
     WSTRING = "999"
 
-    valueChanged = pyqtSignal(int, int)
+    valueChanged = QtCore.pyqtSignal(int, int)
 
     def __init__(self, lowest=0, highest=100, value=0, parent=None):
-        QWidget.__init__(self, parent=parent)
+        QtGui.QWidget.__init__(self, parent=parent)
         self.__lowest = lowest
         self.__highest = highest
-        self.setFocusPolicy(Qt.WheelFocus)
-        self.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding,
-                                       QSizePolicy.Fixed))
-
+        self.setFocusPolicy(QtCore.Qt.WheelFocus)
+        self.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding,
+                                       QtGui.QSizePolicy.Fixed))
+        self.marks = []
         self.setMarkList([dict(value=0, tag='T', Class=TimeMark)])
 
     def setMarkList(self, markList):
         self.markList = markList
-        self.marks = []
+        while self.marks:
+            child = self.marks.pop()
+            child.hide()
+            child.deleteLater()
+
         for ix, dickie in enumerate(markList):
             Class = dickie.pop('Class', (ix and Bookmark or TimeMark))
             value = dickie.pop('value', 0)
@@ -113,10 +111,10 @@ class MarkedSlider(QWidget):
         return self.minimumSizeHint()
 
     def minimumSizeHint(self):
-        font = QFont(self.font())
+        font = QtGui.QFont(self.font())
         font.setPointSize(font.pointSize() - 1)
-        fm = QFontMetricsF(font)
-        return QSize(# fm.width(BookmarkedSlider.WSTRING) *
+        fm = QtGui.QFontMetricsF(font)
+        return QtCore.QSize(# fm.width(BookmarkedSlider.WSTRING) *
                      # (self.__highest - self.__lowest),
                      480,
                      (fm.height() * 2) + self.YMARGIN)
@@ -131,11 +129,11 @@ class MarkedSlider(QWidget):
 
     def mousePressEvent(self, event, child=None):
         # print ('mousePressEvent', self.__class__.__name__, event.x())
-        if event.button() == Qt.LeftButton:
+        if event.button() == QtCore.Qt.LeftButton:
             self.mouseMoveEvent(event, child=child)
             event.accept()
         else:
-            QWidget.mousePressEvent(self, event)
+            QtGui.QWidget.mousePressEvent(self, event)
 
     def mouseMoveEvent(self, event, child=None):
         x = event.x()
@@ -144,7 +142,7 @@ class MarkedSlider(QWidget):
         else:
             x += child.x()
         value = self.valueFromX(x)
-        print ('mouseMoveEvent', self.__class__.__name__, x, value)
+        # print ('mouseMoveEvent', self.__class__.__name__, x, value)
         child.move(x, child.y())
         self.markList[child.ix]['value'] = value
         self.valueChanged.emit(child.ix, value)
@@ -153,17 +151,17 @@ class MarkedSlider(QWidget):
 
     def keyPressEvent(self, event):
         change = 0
-        if event.key() == Qt.Key_Home:
+        if event.key() == QtCore.Qt.Key_Home:
             change = self.__lowest - self.__value
-        elif event.key() in (Qt.Key_Up, Qt.Key_Right):
+        elif event.key() in (QtCore.Qt.Key_Up, QtCore.Qt.Key_Right):
             change = 1
-        elif event.key() == Qt.Key_PageUp:
+        elif event.key() == QtCore.Qt.Key_PageUp:
             change = 10
-        elif event.key() in (Qt.Key_Down, Qt.Key_Left):
+        elif event.key() in (QtCore.Qt.Key_Down, QtCore.Qt.Key_Left):
             change = -1
-        elif event.key() == Qt.Key_PageDown:
+        elif event.key() == QtCore.Qt.Key_PageDown:
             change = -10
-        elif event.key() == Qt.Key_End:
+        elif event.key() == QtCore.Qt.Key_End:
             change = self.__highest - self.__value
         if change:
             value = self.__value
@@ -174,36 +172,36 @@ class MarkedSlider(QWidget):
                 self.update()
             event.accept()
         else:
-            QWidget.keyPressEvent(self, event)
+            QtGui.QWidget.keyPressEvent(self, event)
 
 
     def paintEvent(self, event=None):
-        font = QFont(self.font())
+        font = QtGui.QFont(self.font())
         font.setPointSize(font.pointSize() - 1)
-        fm = QFontMetricsF(font)
+        fm = QtGui.QFontMetricsF(font)
         fracWidth = fm.width(self.WSTRING)
         indent = fm.boundingRect("9").width() / 2.0
         if not X11:
             fracWidth *= 1.5
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing)
-        painter.setRenderHint(QPainter.TextAntialiasing)
-        painter.setPen(self.palette().color(QPalette.Mid))
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.setRenderHint(QtGui.QPainter.TextAntialiasing)
+        painter.setPen(self.palette().color(QtGui.QPalette.Mid))
         painter.setBrush(self.palette().brush(
-                QPalette.AlternateBase))
+                QtGui.QPalette.AlternateBase))
         painter.drawRect(self.rect())
-        segColor = QColor(Qt.green).dark(120)
+        segColor = QtGui.QColor(QtCore.Qt.green).dark(120)
         segLineColor = segColor.dark()
         painter.setPen(segLineColor)
         painter.setBrush(segColor)
         painter.drawRect(self.XMARGIN,
                          self.YMARGIN, self.span(), fm.height())
-        textColor = self.palette().color(QPalette.Text)
+        textColor = self.palette().color(QtGui.QPalette.Text)
         segHeight = fm.height() * 2
         nRect = fm.boundingRect(self.WSTRING)
         yOffset = 0 #  segHeight #  + fm.height()
-        painter.setPen(Qt.yellow)
-        painter.setBrush(Qt.yellow)
+        painter.setPen(QtCore.Qt.yellow)
+        painter.setBrush(QtCore.Qt.yellow)
         #value = self.markList[0]['value']
         #x = self.xFromValue(value)
         x = self.marks[0].x()
@@ -213,15 +211,20 @@ class MarkedSlider(QWidget):
 if __name__ == "__main__":
     import sys
 
-    app = QApplication(sys.argv)
-    form = QDialog()
+    app = QtGui.QApplication(sys.argv)
+    form = QtGui.QDialog()
     # form.setMaximumWidth(80)
-    sliderLabel = QLabel("&Fraction?")
-    slider = MarkedSlider(value=42)
+    sliderLabel = QtGui.QLabel("Time")
+    slider = MarkedSlider()
+    slider2Label = QtGui.QLabel("Speed")
+    slider2 = MarkedSlider()
     sliderLabel.setBuddy(slider)
-    layout = QGridLayout()
+    slider2Label.setBuddy(slider2)
+    layout = QtGui.QGridLayout()
     layout.addWidget(sliderLabel, 0, 0)
     layout.addWidget(slider, 0, 1, 1, 3)
+    layout.addWidget(slider2Label, 1, 0)
+    layout.addWidget(slider2, 1, 1, 1, 3)
     form.setLayout(layout)
 
     def myValueChanged(ix, value):

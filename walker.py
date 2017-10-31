508311f3dea28ce61dedd28a17112383a1a8f2e8
Larry Myerscough <hippos@chello.nl>
2017-10-31 22:51:17 +0100

    looking good! changess in-line images to ccitt image objects; identified rgb images but doesn't try to fix them.

--- walker.py
+++ walker.py
@@ -5,25 +5,17 @@ from subprocess import Popen, PIPE, TimeoutExpired, call, check_output
 from phileas import _html40 as h
 
 class shared:
-    verbosity = 3
+    verbosity = 0
     ps_filename = '/tmp/pdf_fix.ps'
-    #pdf_filename = ''
+    pdf_filename = ''
+    tmp_pdf_filename = '/tmp/pdf_fix.pdf'
 
 def vprint(this_verbosity, *pp, **kw):
     if shared.verbosity >= this_verbosity:
         return print(*pp, **kw)
 
-def fix_images_in_pdf():
-    call("pdf2ps %s %s" %(shared.pdf_filename, shared.ps_filename), shell=True)
-    call("ps2pdf %s %s" %(shared.ps_filename, shared.ok_filename), shell=True)
-
-def images_ok():
-
-    #output = check_output("pdfimages -list %s" % ok_filename, shell=True, close_fds=True)
-    #print(str(output))
-    #return
-    #return call("pdfimages -list %s" % ok_filename, shell=True, close_fds=True)
-    proc = Popen("pdfimages -list %s" % shared.ok_filename, stdout=PIPE, stderr=PIPE, shell=True, close_fds=True)
+def check_images(effort):
+    proc = Popen("pdfimages -list %s" % shared.pdf_filename, stdout=PIPE, stderr=PIPE, shell=True, close_fds=True)
     try:
         outs, errs = proc.communicate(timeout=15)
     except TimeoutExpired:
@@ -32,10 +24,10 @@ def images_ok():
     out_lines = outs.decode(sys.stdout.encoding).split('\n')
     err_lines = errs.decode(sys.stderr.encoding).split('\n')
     if 0: # err_lines:
-        print("(buggy old?) pdfimages gave errors on %s" % shared.ok_filename)
+        print("(buggy old?) pdfimages gave errors on %s" % shared.pdf_filename)
         return None
     if len(out_lines)<2:
-        vprint(1, "not a proper PDF? %s" % shared.ok_filename)
+        vprint(1, "not a proper PDF? %s" % shared.pdf_filename)
         return None  # => no images
     headings = re.split(r'\s+', out_lines.pop(0))
     _hyphens = out_lines.pop(0)
@@ -45,23 +37,30 @@ def images_ok():
     for l in out_lines:
         if len(l)<3:
             continue
-        print(l)
+        vprint(3, l)
         values = re.split(r'\s+', l)
         field =dict(zip(headings, values[1:]))
-        print("field['object']", field['object'])
+        vprint(3, "field['object']", field['object'])
         if field['object']=='[inline]':
-            vprint(2, "%s contains in-line image(s)" % shared.ok_filename)
-            return False  # => not-ok image(s)
-    print("%s is fine!" % shared.ok_filename)
-    return True
+            vprint(1, "%s contains in-line image(s)" % shared.pdf_filename)
+            call("pdf2ps %s %s" % (shared.pdf_filename, shared.ps_filename), shell=True)
+            call("ps2pdf %s %s" % (shared.ps_filename, shared.pdf_filename), shell=True)
+            return True
+        if field['color']!='gray' or int(field['bpc'])!=1:
+            vprint(1, "%s is %u bit(s) %s (not 1 bit gray)" % (shared.pdf_filename, int(field['bpc']), field['color']))
+            call("convert %s -monochrome -threshold 10 %s" % (shared.pdf_filename, shared.tmp_pdf_filename), shell=True)
+            # unfinished!
+            #call("cp %s %s"  % (shared.tmp_pdf_filename, shared.pdf_filename), shell=True)
+            return True
+    vprint(2, "%s is fine!" % shared.pdf_filename)
+    return False
 
 
 def process_pdf():
-    if images_ok() is not False:
-        return
-    fix_images_in_pdf()
-    if images_ok() is not True:
-        print ("%s is still not right after 'fix'." % shared.ok_filename)
+    for effort in range(2):
+        if not check_images(effort):
+            return
+    vprint (1, "%s is still not right after 'fix'." % shared.pdf_filename)
 
 def pdf_walk(path):
     for root, dirs, files in os.walk(path):
@@ -70,13 +69,13 @@ def pdf_walk(path):
             if ext_.lower() not in ('.pdf',):
                 continue
             rel_filename = os.path.join(root, file_)
-            shared.ok_filename = shlex.quote(rel_filename)
-            # print( rel_filename, "->", ok_filename)
+            shared.pdf_filename = shlex.quote(rel_filename)
             process_pdf()
 
 def main():
     prog_path = sys.argv.pop(0)
-    pdf_walk(sys.argv and sys.argv.pop(0) or '.')
+    shared.verbosity = sum([a in ('-v', '--verbose') for a in sys.argv])
+    pdf_walk(sys.argv and (not sys.argv[0].startswith('-')) and sys.argv.pop(0) or '.')
 if __name__ == '__main__':
     print (os.getcwd())
     main()

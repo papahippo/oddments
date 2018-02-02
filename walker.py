1e5db09ad1346a25c2493050cc5f7357990ad657
Larry Myerscough <hippos@chello.nl>
2018-02-02 20:43:37 +0100

    rework of pdf_walker using generic walker now finished and tested.

--- walker.py
+++ walker.py
@@ -1,88 +1,45 @@
 #!/usr/bin/python3
 " dive into music archive and perform maintenance or analyis or report tasks on all files"
-import sys, os, re, shlex
-from subprocess import Popen, PIPE, TimeoutExpired, call, check_output
+import sys, os, shlex
 from phileas import _html40 as h
 
-class shared:
-    verbosity = 0
-    ps_filename = '/tmp/pdf_fix.ps'
-    pdf_filename = ''
-    tmp_pdf_filename = '/tmp/pdf_fix.pdf'
+class Walker:
+    name_ =  "dummy walker"
 
-def vprint(this_verbosity, *pp, **kw):
-    if shared.verbosity >= this_verbosity:
-        return print(*pp, **kw)
+    def __init__(self, prog_path, verbosity=0):
+        self.verbosity = verbosity
+        self.vprint(1, "running", prog_path)
 
-def check_images(effort):
-    proc = Popen("pdfimages -list %s" % shared.pdf_filename, stdout=PIPE, stderr=PIPE, shell=True, close_fds=True)
-    try:
-        outs, errs = proc.communicate(timeout=15)
-    except TimeoutExpired:
-        proc.kill()
-        outs, errs = proc.communicate()
-    out_lines = outs.decode(sys.stdout.encoding).split('\n')
-    err_lines = errs.decode(sys.stderr.encoding).split('\n')
-    if 0: # err_lines:
-        print("(buggy old?) pdfimages gave errors on %s" % shared.pdf_filename)
-        return None
-    if len(out_lines)<2:
-        vprint(1, "not a proper PDF? %s" % shared.pdf_filename)
-        return None  # => no images
-    headings = re.split(r'\s+', out_lines.pop(0))
-    _hyphens = out_lines.pop(0)
-    if not out_lines:
-        vprint(1, "no images in %s" % shared.pdf_filename)
-        return None  # => no images
-    for l in out_lines:
-        if len(l)<3:
-            continue
-        vprint(3, l)
-        values = re.split(r'\s+', l)
-        field =dict(zip(headings, values[1:]))
-        vprint(3, "field['object']", field['object'])
-        if field['object']=='[inline]':  # or field['enc']!='ccitt':
-            vprint(1, "%s contains in-line or not ccitt-encoded image(s)" % shared.pdf_filename)
-            call("pdf2ps %s %s" % (shared.pdf_filename, shared.ps_filename), shell=True)
-            call("ps2pdf %s %s" % (shared.ps_filename, shared.pdf_filename), shell=True)
-            return True
-        if field['color']!='gray' or int(field['bpc'])!=1:
-            vprint(1, "%s is %u bit(s) %s (not 1 bit gray)" % (shared.pdf_filename, int(field['bpc']), field['color']))
-            call("convert %s -monochrome -threshold 50 %s" % (shared.pdf_filename, shared.tmp_pdf_filename), shell=True)
-            # unfinished!
-            #call("cp %s %s"  % (shared.tmp_pdf_filename, shared.pdf_filename), shell=True)
-            return True
-    vprint(2, "%s is fine!" % shared.pdf_filename)
-    return False
+    def vprint(self, this_verbosity, *pp, **kw):
+        if self.verbosity >= this_verbosity:
+            return print(*pp, **kw)
 
-
-def process_pdf(root_, file_):
-    name_, ext_ = os.path.splitext(file_)
-    if ext_.lower() not in ('.pdf',):
+    def handle_file(self, root_, file_):
+        self.vprint(2, self.name_, 'handle_file', root_, file_)
+        rel_filename = os.path.join(root_, file_)
+        self.shl_filename = shlex.quote(rel_filename)
         return False
-# temporary stub!
-    #print (root_,  ' --- ', file_)
-    #return
-    rel_filename = os.path.join(root_, file_)
-    shared.pdf_filename = shlex.quote(rel_filename)
-    for effort in range(2):
-        if not check_images(effort):
-            return
-    vprint (1, "%s is still not right after 'fix'." % shared.pdf_filename)
 
-def dummy_handler(root_, item_):
-    if shared.verbosity:
-        print(root_, item_)
-    return False
+    def handle_dir(self, root_, dir_):
+        self.vprint(2, self.name_, 'handle_dir', root_, dir_)
+        return False
 
-def main(file_handler=dummy_handler, dir_handler=dummy_handler):
+    def walk(self, target):
+        self.vprint(1, "walking through target", target)
+        for root_, dirs_, files_ in os.walk(target):
+            for items_, handler_ in ((dirs_, self.handle_dir), (files_, self.handle_file),):
+                for item_ in items_:
+                    handler_(root_, item_)
+
+def main(class_):
     print (os.getcwd())
     prog_path = sys.argv.pop(0)
-    shared.verbosity = sum([a in ('-v', '--verbose') for a in sys.argv])
-    target = sys.argv and (not sys.argv[0].startswith('-')) and sys.argv.pop(0) or '.'
-    for root_, dirs_, files_ in os.walk(target):
-        for items_, handler_ in ((dirs_, dir_handler), (files_, file_handler),):
-            for item_ in items_:
-                handler_(root_, item_)
+    verbosity = sum([a in ('-v', '--verbose') for a in sys.argv])
+    targets = [arg for arg in sys.argv if not arg.startswith('-')] or '.'
+    instance_ = class_(prog_path, verbosity=verbosity)
+    for target in targets:
+        instance_.walk(target)
+
+
 if __name__ == '__main__':
-    main(file_handler=dummy_handler, dir_handler=dummy_handler)
+    main(Walker)  # our class is both a base class and a dummy class

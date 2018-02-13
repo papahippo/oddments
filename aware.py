ab7010fb4a8890cdbb85a64f43308a45289f6f6a
papahippo <hippos@chello.nl>
2018-02-13 11:55:22 +0100

    included sampe/test 'main' in aware.py.

--- aware.py
+++ aware.py
@@ -4,21 +4,24 @@ print ("so far so good!")
 
 
 class Aware:
+    prev_lineno = -1
+
     def __init__(self, *pp):
         # print ( inspect.getsourcelines(self))
-        top = inspect.getouterframes(inspect.currentframe())[-1]
-        print (*pp, top.code_context)
-        print (top.lineno)
+        last_lineno = inspect.getouterframes(inspect.currentframe())[-1].lineno
+        self.lineno_range = (Aware.prev_lineno+1, last_lineno + 1)
+        Aware.prev_lineno = last_lineno
 
+if __name__ == "__main__":
+    # class 'Aware' is probably best used as a mix-in, but for this example,
+    # let's keep it simple.
 
-print ("the first...")
-aw1 = Aware(42,
-            54,
-            99)
+    aw1 = Aware(42,
+                54,
+                99)
 
-print ("the second...")
-
-Aware(
-    142, 156,
-    167
-)
+    aw2 = Aware(
+        142, 156,
+        167
+    )
+    print ([aw.lineno_range for aw in (aw1, aw2)])

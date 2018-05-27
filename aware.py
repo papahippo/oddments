c03aa230dbc043d19a9bd7705d7e04549a7a66e4
papahippo <hippostech@gmail.com>
2018-05-27 13:59:10 +0200

    taking 'aware.py' on board just to avoid push rejection!

---
+++ aware.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import inspect
+print ("so far so good!")
+
+
+class Aware:
+    prev_lineno = -1
+
+    def __init__(self, *pp):
+        # print ( inspect.getsourcelines(self))
+        last_lineno = inspect.getouterframes(inspect.currentframe())[-1].lineno
+        self.lineno_range = (Aware.prev_lineno+1, last_lineno + 1)
+        Aware.prev_lineno = last_lineno
+
+if __name__ == "__main__":
+    # class 'Aware' is probably best used as a mix-in, but for this example,
+    # let's keep it simple.
+
+    aw1 = Aware(42,
+                54,
+                99)
+
+    aw2 = Aware(
+        142, 156,
+        167
+    )
+    print ([aw.lineno_range for aw in (aw1, aw2)])

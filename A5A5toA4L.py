8e9a060cebf0681d40dfd4d76ba544665a8d37ee
papahippo <hippostech@gmail.com>
2018-05-27 20:41:34 +0200

    tidied up some of the paper-size conversion stuff.

--- A5A5toA4L.py
+++ A5A5toA4L.py
@@ -4,15 +4,16 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 
 from walker import Walker, main
 
-class A5A5toA4(Walker):
+class A5A5toA4L(Walker):
 
-    name_ =  "2xA5 landscape (on 1 A4 page) to 2 A4 pages converter"
+    name_ =  "2xA5 landscape (on 1 A4 page) to 2 A4 landscape pages converter"
+    tag_ = '-A4L'
 
     def handle_item(self, root_, item_, is_dir):
         print(root_, item_)
         Walker.handle_item(self, root_, item_, is_dir)
-        name_, ext_ = os.path.splitext(item_)
-        if is_dir or ext_.lower() not in ('.pdf',):
+        stem_, ext_ = os.path.splitext(item_)
+        if is_dir or ext_.lower() not in ('.pdf',) or stem_.endswith(self.tag_):
             return None
         input = PdfFileReader(open('%s/%s' %(root_, item_), 'rb'))
         output = PdfFileWriter()
@@ -23,9 +24,9 @@ class A5A5toA4(Walker):
             q.mediaBox.lowerRight = (w, h / 2)
             output.addPage(p)
             output.addPage(q)
-        # output.write(sys.stdout)
-        output.write(open('%s/%s-A4%s' %(root_, name_, ext_,), 'wb'))
+        output.write(open('%s/%s%s%s' %(root_, stem_, self.tag_, ext_,), 'wb'))
         return True
 
+
 if __name__ == '__main__':
-    main(A5A5toA4)
+    main(A5A5toA4L)

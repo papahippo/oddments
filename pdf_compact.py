0d602980fc429a70e8cfa86463e5413c430e3aa1
papahippo <hippostech@gmail.com>
2019-07-12 13:47:46 +0200

    significantly simpler approach to coversion to lineart.

--- pdf_compact.py
+++ pdf_compact.py
@@ -3,28 +3,39 @@
 # images within PDFs to lineart/bivalue than that afforded by 'pdf_walker.py'.
 # WARNING: I have abandoned this approach 'for now'!
 #
-import copy, sys, os
-import fitz
-from PIL import Image
-from io import BytesIO
+import sys, os, subprocess
 from walker import Walker
 
-class A5A5toA4L(Walker):
+class Pdf_compact(Walker):
 
     name_ =  "converter of images embedded within a PDF to more compact format"
     tag_ = '-compact'
-    threshold = 50
+    threshold = 0.5
+    resolution = 300
 
     def process_keyword_arg(self, a):
-        if a in ('-t', '--threshold'):
-            self.threshold = int(sys.argv.pop(0))
+        if a in ('-r', '--resolution'):
+            self.resolution = int(sys.argv.pop(0))
             return
+        if a in ('-t', '--threshold'):
+            parts = sys.argv.pop(0).split('%')
+            self.threshold = float(parts.pop(0))
+            if not parts:
+                return
+            self.threshold /= 100.0
+            if not parts.pop(0) and not parts:
+                return
+            raise ValueError("bad threshold value")
+
         if a in ('-h', '--help'):
             print("utility to convert images within a PDF to a more compact format\n"
                 "syntax:  pdf_compact.py [options] [paths]\n"
                   "special options for pdf_compact are (shown quoted but must be entered unquoted!):\n"
                   "'--threshold'   or equivalently '-t'\n"
                   "means interpret the next argument as the black threshold for conversion to mono ('lineart')\n"
+                  "this may be entered as e.g. 0.6 or equivalently 60%. The default is 0.5 (50%)\n"
+                  "'--resolution'   or equivalently '-r'\n"
+                  "means interpret the next argument as the resolution to use. The default is 300.\n"
                   )
         Walker.process_keyword_arg(self, a)
 
@@ -34,35 +45,16 @@ class A5A5toA4L(Walker):
         stem_, ext_ = os.path.splitext(item_)
         if is_dir or ext_.lower() not in ('.pdf',) or stem_.endswith(self.tag_):
             return None
-        input = fitz.open('%s/%s' %(root_, item_))
-        self.vprint(1, "input PDF contains %d page(s)" % len(input))
-        imgcount = 0
-        output = fitz.open()
-        rgb = 'rgb'
-        gray = 'P'
-        for i in range(len(input)):
-            imglist = input.getPageImageList(i)
-            for img in imglist:
-                xref = img[0]  # xref number
-                pix = fitz.Pixmap(input, xref)  # make pixmap from image
-                pilimg = Image.frombuffer(gray, [pix.width, pix.height], pix.samples,
-                                       'raw', gray, 0, 1)
-                imgcount += 1
-                bi_image = pilimg.point(lambda p: p > self.threshold and 255, mode='1')
-                #outputFileName = '%s/%s-%d.tif'  %(root_, stem_, imgcount)
-                #self.vprint(1, 'Writing %s' % outputFileName)
-                bio = BytesIO()
-                bi_image.save(bio, format="tiff", compression="tiff_ccitt")
-                bi_image.save("one.tiff", format="tiff", compression="tiff_ccitt")
-                imgdoc = fitz.open("tiff", bio.getvalue())
-                pdfbytes = imgdoc.convertToPDF()
-                imgpdf = fitz.open("pdf", pdfbytes)
-                page = output.newPage()
-                page.showPDFpage(page.rect, imgpdf, 0)
-        # self.vprint(1, "input PDF contains %d image(s)" % imgcount)
-        output.save('%s/%s%s.pdf' %(root_, stem_, self.tag_))
+        repair_cmd = ('gs -q -dNOPAUSE -dBATCH -dUseCropBox -sOutputFile=%s/%s%s.pdf' %(root_, stem_, self.tag_) +
+                      ' -r%u -sDEVICE=tiffg4 -c "{ %.2f gt { 1 } { 0 } ifelse} settransfer" -f '
+                      % (self.resolution, self.threshold)
+                      + self.shl_pathname)
+        #    ' -c "{ .5 gt { 1 } { 0 } ifelse} settransfer" -f %s' % self.shl_pathname)
+        self.vprint(1, "repair_cmd=", repair_cmd)
+        subprocess.call(repair_cmd, shell=True)
+
         return True
 
 
 if __name__ == '__main__':
-    A5A5toA4L().main()
+    Pdf_compact().main()

3770b60b52d817f3e55961bbdff5f4cca1c2e5f6
papahippo <hippostech@gmail.com>
2019-10-04 16:12:38 +0200

    designed Extra class in one_more_time.py initially to handle at-end-of-file actions when reading by iteration.

--- one_more_time.py
+++ one_more_time.py
@@ -1,5 +1,28 @@
-class OneMoreTime:
-    def __init__(self, MyIteratorClass, extra_value=None, *pp, **kw):
-        self.myIterator = MyIteratorClass(*pp, **kw)
-        self.extra_value = extra_value
+import sys
 
+class Extra:
+    def __init__(self, myIterator, extra=None, *more_extras):
+        self.myIterator = myIterator
+        self.extra_values = list((extra,)+more_extras)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            return next(self.myIterator)
+        except StopIteration:
+            try:
+                return self.extra_values.pop(0)
+            except IndexError:
+                raise StopIteration
+
+def main():
+    this_file = sys.argv[-1]
+    print(this_file)
+    this_file_plus = Extra(open(this_file, 'r')) # , "one more line!\n", "and another!\n")
+    for line in this_file_plus:
+        print(line and line.strip() or "default extra!")
+
+if __name__ == "__main__":
+    main()

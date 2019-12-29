cedf6c5a41d184d10c55023810d98b23555596f6
papahippo <hippostech@gmail.com>
2019-12-29 22:17:22 +0100

    csvo split off. must rework other stuff in oddments around this later?

--- contrib.py
+++ contrib.py
@@ -1,63 +1,13 @@
 #!/usr/bin/python3
 """
 """
-import csv
+import csvo
 from euros import Euros as Money
 from decimal import Decimal, InvalidOperation
 from babel.numbers import parse_decimal
 
 
-class Csv:
-    @classmethod
-    def absorb(cls, infn):
-        cls.instances = []
-        with open(infn, 'r') as csvfile:
-            csv_reader = csv.reader(csvfile, delimiter=';')
-            cls.field_names = csv_reader.__next__()
-            # attr_names = [('_' + name.lower().replace('-', '_')) for name in field_names]
-            cls.python_names = [('_' + name.replace(' ', '_')) for name in cls.field_names]
-            previous = None
-            for row in csv_reader:
-                #print(', '.join(row))
-                instance = cls()
-                instance.previous = previous
-                for an, av in zip(cls.python_names, row):
-                    instance.__setattr__(an, av)
-                instance = instance.intake()
-                if not instance:
-                    continue
-
-                instance.postprocess()
-                cls.instances.append(instance)
-                previous = instance
-
-    @classmethod
-    def exude(cls, outfn):
-        with open(outfn, 'w') as csvfile:
-            csv_writer = csv.writer(csvfile, delimiter=';')
-            csv_writer.writerow(cls.field_names)
-            # print(cls.instances)
-            for inst in cls.instances:
-                inst.giveout(csv_writer)
-
-    def intake(self):
-        return self
-
-    def giveout(self, csv_writer):
-        csv_writer.writerow([getattr(self, name) for name in self.python_names])
-
-    def postprocess(self):
-        return
-
-
-    @classmethod
-    def main(cls, infn=None, outfn=None):
-        if infn:
-            cls.absorb(infn)
-        if outfn:
-            cls.exude(outfn)
-
-class ContribCsv(Csv):
+class ContribCsvo(csvo.Csvo):
 
     perCentIncrease = 10
 
@@ -81,4 +31,4 @@ class ContribCsv(Csv):
 
 
 if __name__ == '__main__':
-    ContribCsv.main('old_rates.csv', 'new_rates.csv')
+    ContribCsvo.main('old_rates.csv', 'new_rates.csv')

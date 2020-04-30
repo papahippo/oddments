12a5b27e81fb9fb3c1264afd7635dc0d2d6d6528
papahippo <hippostech@gmail.com>
2020-04-30 14:30:38 +0200

    at last! mupdf implemetnation of A5A5toA4L.py seems to work exactly as required.

--- A5A5toA4L.py
+++ A5A5toA4L.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-import copy, sys, os
-from PyPDF2 import PdfFileWriter, PdfFileReader, pdf
+import fitz, os
 
 from walker import Walker
-
-root2 = 2.0**0.5
-
 
 class A5A5toA4L(Walker):
 
@@ -36,24 +32,39 @@ class A5A5toA4L(Walker):
         stem_, ext_ = os.path.splitext(item_)
         if is_dir or ext_.lower() not in ('.pdf',) or stem_.endswith(self.tag_):
             return None
-        input = PdfFileReader(open('%s/%s' %(root_, item_), 'rb'))
-        output = PdfFileWriter()
-        for p in [input.getPage(i) for i in range(0, input.getNumPages())]:
-            q = copy.copy(p)
-            (w, h) = p.mediaBox.upperRight
-            #p.scaleBy(1.4142)
-            #p.rotateCounterClockwise(90)
-            print('#p1', p.mediaBox)
-            #p.mediaBox.lowerRight = (w, h / 2)
-            q.mediaBox.upperRight = (w, h / 2)
-            if not self.lower_only:
-                r = pdf.PageObject.createBlankPage(input)
-                r.mergeRotatedScaledPage(p, 90.0, root2)
-                output.addPage(r)
-            if 0:  # not self.upper_only:
-                q.scale(root2, root2)
-                output.addPage(q)
-        output.write(open('%s/%s%s%s' %(root_, stem_, self.tag_, ext_,), 'wb'))
+        print(f'{root_}/{item_}')
+        src = fitz.open(f'{root_}/{item_}')
+        dest = fitz.open()  # output initally empty!
+        for spage in src:  # for each page in input
+            xref = 0  # force initial page copy to output
+            r = spage.rect  # input page rectangle
+            d = fitz.Rect(spage.CropBoxPosition,  # CropBox displacement if not
+                          spage.CropBoxPosition)  # starting at (0, 0)
+            # --------------------------------------------------------------------------
+            # example: cut input page into 2 x 2 parts
+            # --------------------------------------------------------------------------
+            rTop = r - (0, 0, 0, r.height * 0.5)
+            rBottom = r + (0, r.height * 0.5, 0, 0)
+
+            for rx in [rect for rect, exclude in
+                       ((rTop, self.lower_only), (rBottom, self.upper_only)) if not exclude]:
+                rx += d  # add the CropBox displacement
+                page = dest.newPage(-1,  # new output page with rx dimensions
+                                   width=r.height,  # r.width,
+                                   height=r.width)  # r.height)
+                #        width = rx.width,
+                #        height = rx.height)
+                xref = page.showPDFpage(page.rect,  # fill all new page with the image
+                                        src,  # input document
+                                        spage.number,  # input page number
+                                        clip=rx,  # which part to use of input page
+                                        # rotate = 90.0,
+                                        reuse_xref=xref)  # copy input page once only
+
+        # that's it, save output file
+        dest.save(f"{stem_}{self.tag_}{ext_}",
+                 garbage=4,  # eliminate duplicate objects
+                 deflate=True)  # compress stuff where possible
         return True
 
 

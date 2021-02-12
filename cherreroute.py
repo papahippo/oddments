61662de07c1f048eb3a42a51a484c853ac0c4078
Larry Myerscough <hippostech@gmail.com>
2021-02-12 17:54:11 +0100

    committing prior to talng in changes from 'vrolijk';

--- cherreroute.py
+++ cherreroute.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import cherrypy
 from cherrypy.process.plugins import DropPrivileges
 import sys, os, subprocess, shutil, tempfile
@@ -15,15 +15,11 @@ class ReRouter:
         # its return value to the client. Because this is tutorial
         # lesson number 01, we'll just send something really simple.
         # How about...
-        yield f"default: pp={pp} kw= {kw}... "
         url = cherrypy.url()
-        yield f"url={url}... "
         qs = cherrypy.request.query_string
-        yield f"qs={qs}... "
-        reconst = '/'.join(pp)
-        if kw:
-            reconst += '?'+'&'.join([f"{var}={val}" for var, val in kw.items()])
-        yield f"reconst={reconst}... "
+        new_url = url.replace('http://', 'https://') + (f"?{qs}" if qs else "")
+        yield f"rerouting to {new_url}"
+        raise cherrypy.HTTPRedirect(new_url)
 
 def main():
     print(f"running'main' in {__file__}")
@@ -34,7 +30,7 @@ def main():
     server_prog_name = sys.argv.pop(0) if sys.argv else '[unknown]'
     global_config = {
         'server.socket_host': local_ip_address,
-        'server.socket_port': 8080,
+        'server.socket_port': 80,
         'server.thread_pool': 10,
     }
 

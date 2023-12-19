1b0b3c1e795d41f2187874b99dc8044de1b5c5cf
papahippo <hippostech@gmail.com>
2023-12-19 15:25:14 +0100

    fixed pycharm related files

--- retabulate.py
+++ retabulate.py
@@ -7,7 +7,7 @@ input_file_name = sys.argv.pop(0) if sys.argv else '/home/gill/tmp/mexico_music.
 td_cells = [f"(h.td % '{line.strip()}')" for line in open(input_file_name, 'r') ]
 n_rows = len(td_cells) // 2
 #print(td_cells)
-rows = [f"h.tr % ({td_cells[i_row*2]} + {td_cells[1+i_row*2]}) +"for i_row in range(n_rows)]
+rows = [f"h.tr % ({td_cells[i_row*2]} + {td_cells[1+i_row*2]}) +" for i_row in range(n_rows)]
 row_list = '\n'.join(rows)
 table = f"h.table % ({row_list})"
 print(table)

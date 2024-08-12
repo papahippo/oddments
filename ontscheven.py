3fe59c266605e9c71a0ab0406d0f3ead85cbb94c
Larry Myerscough <hippostech@gmail.com>
2024-08-12 16:50:31 +0200

    tinkered with ontscheven.py ... maybe this is a good approach for fixing score images?

--- ontscheven.py
+++ ontscheven.py
@@ -4,5 +4,7 @@ quicky to correct an image that was scanned in at a slight angle.
 """
 from PIL import Image
 im = Image.open("./images/Wim-duet-1.png")
-im_new = im.rotate(-3.3, expand=True, fillcolor="white")
+im_new = im.rotate(-3.2, expand=True, fillcolor="white")
 im_new.save('./images/im_new.png', 'png')
+im_mono = im_new.convert('1')
+im_mono.save('./images/im_mono.png', 'png')
